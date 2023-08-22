471ba4b3c4f502b231692703322a0038c4138093
Maruf Hasan <marufhasanmitul@gmail.com>
2023-08-22 23:52:04 +0600

    Google Location Api Integration Done

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,6 +15,14 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Gooogle Location")
+      ),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+            target: LatLng(25.740580, 89.261140),
+            zoom: 11.0
+
+        ),
+
       ),
     );
   }
